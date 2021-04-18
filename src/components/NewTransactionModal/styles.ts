cb1f6436aa98a99f926e30e891b97bcb26bd52ed
lucasbaquinoo <lucasbaquino@icloud.com>
2021-04-18 11:11:07 -0300

    increment state and props to buttons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -85,3 +85,37 @@ export const TransactionTypeContainer = styled.div`
     }
   }
 `
+
+interface RadioBoxProps {
+  isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+  background: ${(props) => props.isActive ? '#fff' : 'transparent'};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')};
+  }
+
+  img {
+    height: 20px;
+    width: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+`
