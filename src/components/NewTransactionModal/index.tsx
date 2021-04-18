283ca975c47813ebaf9ac6af118c33c9e6d94edd
lucasbaquinoo <lucasbaquino@icloud.com>
2021-04-18 14:12:19 -0300

    resolve the actions to add a new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,7 +4,6 @@ import Modal from "react-modal"
 import closeImg from "../../assets/close.svg"
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
-import { api } from "../../services/api"
 import { TransactionsContext } from "../../TransactionsContext"
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles"
@@ -22,15 +21,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   const [category, setCategory] = useState('');
   const [type, setType] = useState("deposit");
 
-  function handleCreateTransactionModal(event: FormEvent) {
+  async function handleCreateTransactionModal(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type
     });
+
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose();
   }
 
   return (
